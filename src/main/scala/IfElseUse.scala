e89f2db9cd30b2052190bd9f1fdbc2a69f372020
vikasprajapati42027 <vikas.prajapati2027@gmail.com>
2021-12-24 12:26:16 +0530

    Added two new Scala pro

---
+++ src/main/scala/IfElseUse.scala
@@ -0,0 +1,14 @@
+object IfElseUse {
+    def main(args:Array[String]): Unit =
+    {
+        var a:Int=10;
+        if(a==10)
+            {
+                println("Value 10 is Available")
+            }
+        else
+            {
+                println("Value not Available")
+            }
+    }
+}
